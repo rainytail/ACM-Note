8f43cf1551f6f4e2e190d657b2391b29e62a8f5b
horb7 <1834036790@qq.com>
2022-04-02 09:13:48 +0800

    add bit

---
+++ 杂项/位运算相关.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+https://oi-wiki.org/math/bit/
+
+位运算常用等式
+a|b = (a^b) + (a&b)    a^b = (a|b) - (a&b)
+a+b = (a|b) + (a&b)
+    = (a&b)*2 + (a^b)
+    = (a|b)*2 - (a^b)
+(a&b)^(a&c) = a&(b^c)
+
+运算符优先级  https://www.sojson.com/operation/cxx.html
+
+位运算 子集枚举
+二进制枚举  [0, (1>>n) - 1]
+
+S ∪ {i}: S|1<<i
+S ∩ T: S&T
+S \ {i}: S&^(1<<i)
+
+检测是否状态内只存在一个0: S&(S-1) == 0
+
+异或和相关
+https://www.luogu.com.cn/problem/P4462 异或前缀
+*/
+
+/*
+bitset用法：bitset一个模板类，相当于若干个位合在一起，存储每一位用一个bit，所以常数为1/32
+参考  https://en.cppreference.com/w/cpp/utility/bitset
+*/
+
+/*
+c++内置函数
+参考  https://blog.csdn.net/jasonchen_gbd/article/details/44948523
+__builtlin_ffs(x) x最后一位为1的位是从前往后的第几位
+__builtlin_popcount(x) 返回x二进制下1的个数, 注意x为unsigned int类型  x为long long --> __builtlin_popcountll
+__builtlin_ctz(x) x末尾0的个数
+__builtlin_clz(x) x前导0的个数
+__builtlin_parity(x) x二进制下1的奇偶性
+*/
+
+int main()
+{
+    return 0;
+}
